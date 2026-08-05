87bd00ae2fc2a8b8e514e38aba3c01c0fe8b5183
Mariana Rosa <marianarsamaral@gmail.com>
2026-08-05 18:12:33 -0300

    Initial commit

---
+++ src/components/Dialog/index.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from "react"
+import  './dialog.style.css'
+import { IconClose } from '../icons'
+
+export function Dialog( {isOpen, onClose, children} ) {
+    const dialogRef = useRef(null)
+
+    useEffect(() => {
+        console.log('Deveriamos mostrar o modal?', isOpen)
+        if (isOpen) {
+            openDialog() 
+        } else {
+            closeDialog()
+        }
+    },[isOpen])
+
+    const openDialog = () => {
+        dialogRef.current.showModal();
+    };
+
+    const closeDialog = () => {
+        dialogRef.current.close();
+    };
+
+
+
+    return (
+        <>
+            <dialog ref={dialogRef} className="dialog">
+                <div className="btn-close-wrapper">
+                    <button autoFocus onClick={onClose} className="btn-close"> 
+                        <IconClose/> 
+                    </button>
+                </div>
+                {children}
+            </dialog>
+        </>
+    )
+}
